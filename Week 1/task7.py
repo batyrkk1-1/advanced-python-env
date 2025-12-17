2367dcd226a71212c8e42715e8f1cce69b71f4b5
batyrkk1-1 <batyrkk1@gmail.com>
2025-12-17 13:09:42 +0500

    Update task7.py

--- Week 1/task7.py
+++ Week 1/task7.py
@@ -1,1 +1,12 @@
+a = float(input())
+op = input()
+b = float(input())
+
+print(
+    a + b if op == '+' else
+    a - b if op == '-' else
+    a * b if op == '*' else
+    "Division by zero" if op == '/' and b == 0 else
+    a / b
+)
 

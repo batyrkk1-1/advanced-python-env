b962a64d82b11584ab712ee2f99b43ea5d88e7d7
batyrkk1-1 <batyrkk1@gmail.com>
2025-12-17 13:08:00 +0500

    Update task6.py

--- Week 1/task6.py
+++ Week 1/task6.py
@@ -1,1 +1,13 @@
+a = float(input("First number: "))
+op = input("Operation: ")
+b = float(input("Second number: "))
+
+if op == '+':
+    print(a + b)
+elif op == '-':
+    print(a - b)
+elif op == '*':
+    print(a * b)
+elif op == '/':
+    print(a / b)
 
